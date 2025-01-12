270f025de553b0e0c276ca269be7cf17eb15003e
hogi2020 <junmyung2020@gmail.com>
2025-01-13 02:31:24 +0900

    react start

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,10 @@
+import 'react'
+import { Link } from 'react-router-dom'
+
+const Home = () => {
+    return (
+        <Link to='/login'>Home</Link>
+    )
+}
+
+export default Home
---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,9 @@
+import 'react'
+
+const login = () => {
+    return (
+        <div>login</div>
+    )
+}
+
+export default login
