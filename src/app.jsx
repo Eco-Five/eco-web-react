feb02f309283b6e0c982714e45db8e833c16f1b3
hoon0711 <doh40711@gmail.com>
2025-01-15 21:11:17 +0900

    1차 머지

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,5 @@
 import { lazy, Suspense } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
 
 import Header from './components/section/Header';
 import Footer from './components/section/Footer';
@@ -8,22 +8,41 @@ import MyPage from "./pages/mypage/MyPage";
 
 const Home = lazy(() => import("./pages/home/Home"));
 const Login = lazy(() => import("./pages/login/Login"));
-const FindAuth = lazy(() => import("./pages/findAuth/FindAuth"));
+const Find = lazy(() => import("./pages/find/Find"));
+
+
+//============= Header와 Footer를 제외한 레이아웃===============//
+function AppLayout({ children }) {
+  const location = useLocation();
+
+  // FindAuth 페이지에서 Header와 Footer를 제외
+  const excludeHeaderFooter = location.pathname === "/login/find";
+
+  return (
+    <>
+      {!excludeHeaderFooter && <Header />}
+      <>{children}</>
+      {!excludeHeaderFooter && <Footer />}
+    </>
+  );
+}
+//============= Header와 Footer를 제외한 레이아웃===============//
+
 
 
 function App() {
   return (
     <BrowserRouter>
       <Suspense fallback={<div>Loading...</div>}>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path='/about' element={<About />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/login/find" element={<FindAuth />} />
-          <Route path="/mypage" element={<MyPage />} />
-        </Routes>
-        <Footer />
+        <AppLayout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/login/find" element={<Find />} />
+            <Route path="/mypage" element={<MyPage />} />
+          </Routes>
+        </AppLayout>
       </Suspense>
     </BrowserRouter>
   );
